6ec59b3a7a94b744ead850e5fab320c7081cf22d
Yuri All Doctors <yuri@alldoctors.com.br>
2018-10-09 16:51:34 -0300

    Update

---
+++ src/app/customers/customer-details/customer-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
+
+@Component({
+  selector: 'customer-details',
+  templateUrl: './customer-details.component.html',
+  styleUrls: ['./customer-details.component.css']
+})
+export class CustomerDetailsComponent implements OnInit {
+
+  @Input() customer: Customer;
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+  }
+
+  updateActive(isActive: boolean) {
+    this.customerService.updateCustomer(this.customer.key, { active: isActive });
+  }
+
+  deleteCustomer() {
+    this.customerService.deleteCustomer(this.customer.key);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBIVNcUn_OY4xtUXlUAB2TuQ6_pu6D5WOI",
+    authDomain: "teste-29d73.firebaseapp.com",
+    databaseURL: "https://teste-29d73.firebaseio.com/",
+    projectId: "teste-29d73",
+    storageBucket: "gs://teste-29d73.appspot.com",
+    messagingSenderId: "340515382596"
+  }
 };
+
 
 /*
  * In development mode, to ignore zone related error stack frames such as
